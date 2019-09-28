5ccd1cbf323beb6f62c40f35cd4263a1a82bd93b
ivantesker <teskerivan.t@gmail.com>
2019-09-28 23:10:48 +0300

    f

--- back/app/app.py
+++ back/app/app.py
@@ -145,7 +145,7 @@ api.add_resource(Stations, '/stations')
 #   return send_from_directory('map/', path)
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=666 , debug=True)
+     app.run(host='0.0.0.0', port=5050 , debug=True)
 
 @app.errorhandler(500)
 def server_error(e):
