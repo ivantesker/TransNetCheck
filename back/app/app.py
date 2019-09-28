a0b13541876dac40629a6502501e6a48e5f69dd0
ivantesker <teskerivan.t@gmail.com>
2019-09-28 22:45:52 +0300

    a

--- back/app/app.py
+++ back/app/app.py
@@ -1,25 +1,36 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 from flask_restful import Resource, Api, reqparse, abort
+from flask_cors import CORS
 from sqlalchemy import create_engine
 from json import dumps
 import time
 import math
+import requests
+import json
+
 e = create_engine('sqlite:///db/tnc.db')
+
+city_api = "https://geocode-maps.yandex.ru/1.x/"
+
+stations_api = "https://api.rasp.yandex.net/v3.0/nearest_stations/"
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 parser = reqparse.RequestParser()
-parser.add_argument('latitude', type=float, required=True, location='form')
-parser.add_argument('longtitude', type=float,  required=True, location='form')
-parser.add_argument('level', type=int, required=True, location='form')
-parser.add_argument('checksum', type=int, required=True, location='form')
-parser.add_argument('operator', type=str, required = True, location='form')
-parser.add_argument('type', type=str, location='form')
+parser.add_argument('latitude', type=float, required=True, location='args')
+parser.add_argument('longtitude', type=float,  required=True, location='args')
+parser.add_argument('level', type=int, required=True, location='args')
+parser.add_argument('checksum', type=int, required=True, location='args')
+parser.add_argument('operator', type=str, required = True, location='args')
+parser.add_argument('type', type=str, location='args')
 
+stat_parser = reqparse.RequestParser()
+stat_parser.add_argument('city', type=str, location='args')
 
 def check_request(data):
-    print(data['latitude'])
+    # print(data['latitude'])
     # print(data['longtitude'])
     # print(data['level'])
     # print(data['checksum'])
@@ -30,6 +41,42 @@ def check_request(data):
     #     return False
     return True
 
+# def get_coordinate(data):
+#     pass
+    
+def form_query(data):
+    pass
+    
+def get_center(points):
+    up = points['upperCorner'].split(' ')
+    low = points['lowerCorner'].split(' ')
+    return (float(up[0]) + float(low[0])) / 2, (float(up[1]) + float(low[1])) / 2
+
+def get_city_points(d):
+    try:
+        points = d['response']['GeoObjectCollection']['metaDataProperty']['GeocoderResponseMetaData']['boundedBy']['Envelope']
+    except:
+        points = d['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['boundedBy']['Envelope']
+    return points
+    
+def get_json_response(api, payload):
+    r = requests.get(api, params=payload).text
+    d = json.loads(r)
+    return d
+
+def form_stations(d):
+    a = d['stations']
+    stations = dict()
+    titles = []
+    codes = []
+    for i in a:
+        stations[i['title']] = i['code']
+    #     titles.append(i['title'])
+    #     codes.append(i['code'])
+    # stations.append(titles)
+    # stations.append(codes)
+    return stations
+
 class Signals(Resource):
     def get(self):
         conn = e.connect()
@@ -37,9 +84,10 @@ class Signals(Resource):
 
         return {'signals': [i for i in query.cursor.fetchall()]}
 
-    def put(self):
+    def post(self):
         args = parser.parse_args()
         conn = e.connect()
+        # print(response.text)
         if args['type']:
             command = "INSERT INTO signals(latitude, longtitude, level, date, operator, ntype) VALUES({latitude}, {longtitude}, {level}, {date}, {operator}, {ntype})"\
                 .format(latitude=args['latitude'], longtitude=args['longtitude'], level=args['level'], date=int(time.time()), operator=args['operator'].lower(), ntype=args['type'])
@@ -55,14 +103,50 @@ class Signals(Resource):
     def delete(self):
         pass
 
-    def post(self):
+    def put(self):
         pass
- 
-api.add_resource(Signals, '/signals')
+
+class Stations(Resource):
+    def get(self):
+        args = stat_parser.parse_args()
+        stations = []
+        city_load = {'apikey': '9491694e-e68b-4127-b2ce-272871fde6a2', 'geocode': args['city'].replace("\"",''), 'format':'json'}
+        # print(city_load)
+        d = get_json_response(city_api, city_load)
+        # print(d)
+        points = get_city_points(d)
+        lon, lat = get_center(points)
+        # print(lon, lat)
+        stations_load = {'apikey': 'f88c2a51-4a12-4072-bf65-4da9626ea175', 'format':'json', 'lng': str(lon), 'lat': str(lat), 'distance': '30', 'limit':'50','transport_types':'train' }
+        d = get_json_response(stations_api, stations_load)
+        return form_stations(d)
+
+
+@app.route('/<path:path>', methods=['GET'])
+def static_proxy(path):
+  return send_from_directory('./map/', path)
 
 @app.route('/')
+def root():
+  return send_from_directory('./map/', 'index.html')
+
+@app.route('/home')
 def index():
     return render_template('index.html')
 
+api.add_resource(Signals, '/signals')
+
+api.add_resource(Stations, '/stations')
+
+
+
+# @app.route('/<path:path>', methods=['GET'])
+# def static_proxy(path):
+#   return send_from_directory('map/', path)
+
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=666)
+     app.run(host='localhost', port=555, debug=True)
+
+@app.errorhandler(500)
+def server_error(e):
+  return 'An internal error occurred [main.py] %s' % e, 500
---
+++ back/app/map/scripts.js
@@ -0,0 +1,78 @@
+
+function GetMap(json) {
+    ymaps.ready(function init() {
+        var myMap = new ymaps.Map('map', {
+            center: [55.733835, 37.588227],
+            zoom: 11,
+            controls: [
+                "zoomControl"
+            ]
+        });
+        var objectManager = new ymaps.ObjectManager();
+        // Добавляем описание объектов в формате JSON в менеджер объектов.
+        objectManager.add(json);
+        // Добавляем объекты на карту.
+        myMap.geoObjects.add(objectManager);
+
+        myMap.controls.get('zoomControl').options.set('position', {
+            bottom: "35vh",
+            right: "15px"
+        });
+
+        var searchControls = [];
+
+        searchControls.push(new ymaps.control.SearchControl({
+            options: {
+                kind: 'locality',
+                placeholderContent: "Город отправления",
+                position: {
+                    left: "15px",
+                    top: "10px"
+                }
+            }
+        }));
+
+        searchControls.push(new ymaps.control.SearchControl({
+
+            options: {
+                kind: 'locality',
+                placeholderContent: "Город прибытия",
+                position: {
+                    left: "15px",
+                    top: "40px"
+                }
+            }
+        }));
+
+        searchControls.push(new ymaps.control.SearchControl({
+
+            options: {
+                kind: 'locality',
+                placeholderContent: "Город прибытия",
+                position: {
+                    left: "15px",
+                    top: "40px"
+                }
+            }
+        }));
+
+        searchControls.push(new ymaps.control.SearchControl({
+
+            options: {
+                kind: 'locality',
+                placeholderContent: "Город прибытия",
+                position: {
+                    left: "15px",
+                    top: "40px"
+                }
+            }
+        }));
+
+        searchControls.forEach(sc => {
+            myMap.controls.add(sc);
+        });
+
+    });
+}
+;
+//# sourceMappingURL=scripts.js.map
