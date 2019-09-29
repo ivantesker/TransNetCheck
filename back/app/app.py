c9c7bbf074ff50a592780378cdbeb7ce970dd9ff
ivantesker <teskerivan.t@gmail.com>
2019-09-29 06:41:00 +0300

    a

--- back/app/app.py
+++ back/app/app.py
@@ -16,24 +16,32 @@ stations_api = "https://api.rasp.yandex.net/v3.0/nearest_stations/"
 
 route_api = "https://api.rasp.yandex.net/v3.0/search/"
 
+route_stations_api = "https://api.rasp.yandex.net/v3.0/thread/"
+
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
 parser = reqparse.RequestParser()
-parser.add_argument('latitude', type=float, required=True, location='args')
-parser.add_argument('longtitude', type=float,  required=True, location='args')
-parser.add_argument('level', type=int, required=True, location='args')
-parser.add_argument('checksum', type=int, required=True, location='args')
-parser.add_argument('operator', type=str, required = True, location='args')
-parser.add_argument('type', type=str, location='args')
+parser.add_argument('latitude', type=float, required=True, location='form')
+parser.add_argument('longtitude', type=float,  required=True, location='form')
+parser.add_argument('level', type=int, required=True, location='form')
+parser.add_argument('checksum', type=int, required=True, location='form')
+parser.add_argument('operator', type=str, required = True, location='form')
+parser.add_argument('type', type=str, location='form')
 
 stat_parser = reqparse.RequestParser()
 stat_parser.add_argument('city', type=str, required = True, location='args')
 
 route_parser = reqparse.RequestParser()
-route_parser.add_argument('sfrom', type=str, required = True, location='args')
-route_parser.add_argument('tfrom', type=str, required = True, location='args')
+route_parser.add_argument('start_point', type=str, required = True, location='args')
+route_parser.add_argument('end_point', type=str, required = True, location='args')
+route_parser.add_argument('number', type=str,  location='args')
+route_parser.add_argument('title', type=str,  location='args')
+
+
+# test
+coordinates = dict()
 
 def check_request(data):
     # print(data['latitude'])
@@ -77,10 +85,6 @@ def form_stations(d):
     codes = []
     for i in a:
         stations[i['title']] = i['code']
-    #     titles.append(i['title'])
-    #     codes.append(i['code'])
-    # stations.append(titles)
-    # stations.append(codes)
     return stations
 
 class Signals(Resource):
@@ -93,7 +97,7 @@ class Signals(Resource):
     def put(self):
         args = parser.parse_args()
         conn = e.connect()
-        # print(response.text)
+        print(request.data)
         if args['type']:
             command = "INSERT INTO signals(latitude, longtitude, level, date, operator, ntype) VALUES({latitude}, {longtitude}, {level}, {date}, {operator}, {ntype})"\
                 .format(latitude=args['latitude'], longtitude=args['longtitude'], level=args['level'], date=int(time.time()), operator=args['operator'].lower(), ntype=args['type'])
@@ -110,14 +114,19 @@ class Signals(Resource):
         pass
 
     def post(self):
-        args = parser.parse_args()
+        # print(json.loads(request.data))
+        
+        args = json.loads(request.data.decode('UTF-8').replace("'", '"'))
+        # parser.parse_args()
         conn = e.connect()
-        # print(response.text)
+        print('OK')
+        print(request)
         if args['type']:
-            command = "INSERT INTO signals(latitude, longtitude, level, date, operator, ntype) VALUES({latitude}, {longtitude}, {level}, {date}, {operator}, {ntype})"\
-                .format(latitude=args['latitude'], longtitude=args['longtitude'], level=args['level'], date=int(time.time()), operator=args['operator'].lower(), ntype=args['type'])
+            command = "INSERT INTO signals(latitude, longtitude, level, date, operator, ntype) VALUES({latitude}, {longtitude}, {level}, {date}, '{operator}', '{ntype}')"\
+                .format(latitude=args['latitude'], longtitude=args['longtitude'], level=args['level'], date=int(time.time()), operator=args['operator'].lower(), ntype=str(args['type']))
+            print(command)
         else:
-            command = "INSERT INTO signals(latitude, longtitude, level, date, operator) VALUES({latitude}, {longtitude}, {level}, {date}, {operator})"\
+            command = "INSERT INTO signals(latitude, longtitude, level, date, operator) VALUES({latitude}, {longtitude}, {level}, {date}, '{operator}')"\
                 .format(latitude=args['latitude'], longtitude=args['longtitude'], level=args['level'], date=int(time.time()), operator=args['operator'].lower())
         if check_request(args):
             query = conn.execute(command)
@@ -143,9 +152,46 @@ class Stations(Resource):
 class Routes(Resource):
     def post(self):
         args = route_parser.parse_args()
-        route_load = {'apikey': 'f88c2a51-4a12-4072-bf65-4da9626ea175', 'format':'json', 'from': args['sfrom'], 'to': args['tfrom'], 'transport_types':'train'}
-        d = get_json_response(route_api, route_load)
-        return d
+        route_load = {'apikey': 'f88c2a51-4a12-4072-bf65-4da9626ea175', 'format':'json', 'from': args['start_point'], 'to': args['end_point'], 'transport_types':'train'}
+        d = get_json_response(route_api, route_load)['segments']
+
+        uids = []
+        for i in d:
+            ro = str(i['thread']['number']) + " " + str(i['thread']['title'])
+            if ro not in uids:
+                uids.append(ro)
+        return uids
+    
+    def put(self):
+        args = route_parser.parse_args()
+        route_load = {'apikey': 'f88c2a51-4a12-4072-bf65-4da9626ea175', 'format':'json', 'from': args['start_point'], 'to': args['end_point'], 'transport_types':'train'}
+        d = get_json_response(route_api, route_load)['segments']
+        
+        threads = []
+
+        for i in d:
+            if i['thread']['number'] != args['number']:
+                continue
+            route_stations_load= {'apikey': 'f88c2a51-4a12-4072-bf65-4da9626ea175', 'format':'json', 'uid': i['thread']['uid']}
+            e = get_json_response(route_stations_api, route_stations_load)
+            threads = []
+            for j in e['stops']:
+                if j['station']['title'] in coordinates:
+                    [lon, lat] = coordinates[j['station']['title']]
+                else:
+                    city_load = {'apikey': '9491694e-e68b-4127-b2ce-272871fde6a2', 'geocode': j['station']['title'], 'format':'json', 'results':'1'}
+                    d = get_json_response(city_api, city_load)
+                    points = get_city_points(d)
+                    lon, lat = get_center(points)
+                    coordinates[j['station']['title']] = [lon, lat]
+                station_meta = []
+                station_meta.append(lon)
+                station_meta.append(lat)
+                threads.append(station_meta)
+        if threads:
+            return threads
+        else:
+            return {'message': 'Error'}
 
 @app.route('/<path:path>', methods=['GET'])
 def static_proxy(path):
@@ -164,12 +210,6 @@ api.add_resource(Signals, '/signals')
 api.add_resource(Stations, '/stations')
 
 api.add_resource(Routes, '/routes')
-
-
-
-# @app.route('/<path:path>', methods=['GET'])
-# def static_proxy(path):
-#   return send_from_directory('map/', path)
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port=999 , debug=True)
