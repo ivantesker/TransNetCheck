9ff7ad15630d93d0f2d5da83d6437988bededba2
ReC <kitanin.ser@mail.ru>
2019-09-29 05:30:35 +0300

    updated map component and getMap service

--- front/src/app/map/map.component.ts
+++ front/src/app/map/map.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-declare const getCustomSuggestions: any;
+declare const getCitiesSuggestions: any;
+declare const getSuggestionsOfStArrival: any;
+declare const getSuggestionsOfTrainFlight: any;
+declare const getSuggestionsOfStDeparture: any;
+declare const flightRouteData: any;
 declare const GetMap: any;
+declare const AddObjectsToMap: any;
 
 @Component({
   selector: 'app-map',
@@ -15,17 +20,59 @@ export class MapComponent implements OnInit {
 
   }
   title = 'HackMap';
-
   ngOnInit() {
-    this._getMap();
+    this.initMap();
+
     this.getCustomSuggestions();
   }
-  public getCustomSuggestions() {
-    getCustomSuggestions();
+  public async getCustomSuggestions() {
+    getCitiesSuggestions();
   }
 
-  public _getMap() {
-    this.http.get('../data/info.json').subscribe((data) => GetMap(data));
+  public selectFromCityInput() {
+    const fromCityInput = document.getElementById('fromCity')['value'];
+    if (fromCityInput === '') {
+      return;
+    }
+    getSuggestionsOfStDeparture(fromCityInput.trim());
+  }
+
+  public selectToCityInput() {
+    const toCityInput = document.getElementById('toCity')['value'];
+    if (toCityInput === '') {
+      return;
+    }
+    getSuggestionsOfStArrival(toCityInput.trim());
+  }
+  public async selectTrainFlight() {
+    let trainFlightFrom = null;
+    let trainFlightTo = null;
+    for (const key in flightRouteData.arrayStDeparture) {
+      if (flightRouteData.arrayStDeparture.hasOwnProperty(key)) {
+        if (document.getElementById('stDeparture')['value'] === key) {
+          trainFlightFrom = flightRouteData.arrayStDeparture[key];
+          break;
+        }
+      }
+    }
+    for (const key in flightRouteData.arrayStArrival) {
+      if (flightRouteData.arrayStArrival.hasOwnProperty(key)) {
+        if (document.getElementById('stArrival')['value'] === key) {
+          trainFlightTo = flightRouteData.arrayStArrival[key];
+          break;
+        }
+      }
+    }
+    if (trainFlightFrom == null || trainFlightTo == null) {
+      return;
+    }
+    getSuggestionsOfTrainFlight(trainFlightFrom.trim(), trainFlightTo.trim());
+  }
+  public initMap() {
+    GetMap();
+  }
+  public addObjectsToMap() {
+    this.http.get('../data/info.json').subscribe((data) => AddObjectsToMap(data));
   }
 
 }
--- front/src/services/getMap.js
+++ front/src/services/getMap.js
@@ -1,5 +1,6 @@
+var apiURL = 'http://977ff9d0.ngrok.io';
 
-function GetMap(json) {
+function GetMap() {
     ymaps.ready(function init() {
         var myMap = new ymaps.Map('map', {
             center: [55.733835, 37.588227],
@@ -8,11 +9,6 @@ function GetMap(json) {
                 "zoomControl"
             ]
         });
-        var objectManager = new ymaps.ObjectManager();
-        // Добавляем описание объектов в формате JSON в менеджер объектов.
-        objectManager.add(json);
-        // Добавляем объекты на карту.
-        myMap.geoObjects.add(objectManager);
 
         myMap.controls.get('zoomControl').options.set('position', {
             bottom: "35vh",
@@ -20,76 +16,22 @@ function GetMap(json) {
         });
     });
 }
-
-var stDeparture = [
-    "Эйс/Бэт-пес",
-    "Эретик (Левиафан)",
-    "Элемент Герл",
-    "Эндрю Беннет",
-    "Эквинокс",
-    "Юга Хан",
-    "Юошио Натсуми",
-    "Ядовитый Плющ",
-    "Ястреб и Голубь",
-    "B’wana Beast",
-    "Bat Lash",
-    "Bernadeth",
-    "Blackbriar Thorn",
-    "Pow Wow Smith",
-    "Machiste"];
-var stArrival = [
-    "Эйс/Бэт-пес",
-    "Эретик (Левиафан)",
-    "Элемент Герл",
-    "Эндрю Беннет",
-    "Эквинокс",
-    "Юга Хан",
-    "Юошио Натсуми",
-    "Ядовитый Плющ",
-    "Ястреб и Голубь",
-    "B’wana Beast",
-    "Bat Lash",
-    "Bernadeth",
-    "Blackbriar Thorn",
-    "Pow Wow Smith",
-    "Machiste"];
-var trainFlight = [
-    "Эйс/Бэт-пес",
-    "Эретик (Левиафан)",
-    "Элемент Герл",
-    "Эндрю Беннет",
-    "Эквинокс",
-    "Юга Хан",
-    "Юошио Натсуми",
-    "Ядовитый Плющ",
-    "Ястреб и Голубь",
-    "B’wana Beast",
-    "Bat Lash",
-    "Bernadeth",
-    "Blackbriar Thorn",
-    "Pow Wow Smith",
-    "Machiste"];
-
+function AddObjectsToMap(json) {
+    ymaps.ready(function init() {
+        var objectManager = new ymaps.ObjectManager();
+        // Добавляем описание объектов в формате JSON в менеджер объектов.
+        objectManager.add(json);
+        // Добавляем объекты на карту.
+        myMap.geoObjects.add(objectManager);
+    });
+}
 var find = function (arr, find) {
     return arr.filter(function (value) {
         return (value + "").toLowerCase().indexOf(find.toLowerCase()) != -1;
     });
 };
-function getDataProvider(arr) {
-    return {
-        suggest: function (request, options) {
-            var res = find(arr, request),
-                arrayResult = [],
-                results = Math.min(options.results, res.length);
-            for (var i = 0; i < results; i++) {
-                arrayResult.push({ displayName: res[i], value: res[i] })
-            }
-            return ymaps.vow.resolve(arrayResult);
-        }
-    };
-}
 
-function getCustomSuggestions() {
+function getCitiesSuggestions() {
     ymaps.ready(function init() {
         var suggestView1 = new ymaps.SuggestView('toCity', {
             results: 5
@@ -97,17 +39,96 @@ function getCustomSuggestions() {
         var suggestView2 = new ymaps.SuggestView('fromCity', {
             results: 5
         });
-        var suggestView2 = new ymaps.SuggestView('stDeparture', {
-            results: 5,
-            provider: getDataProvider(stDeparture)
+    });
+}
+
+flightRouteData = {
+    arrayStArrival: [],
+    arrayStDeparture: []
+};
+
+function getSuggestionsOfStDeparture(stDeparture) {
+    ymaps.ready(function init() {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                flightRouteData['arrayStDeparture'] = JSON.parse(xmlHttp.responseText);
+            }
+        }
+        xmlHttp.open("GET", apiURL + `/stations?city="${stDeparture}"`, true);
+        xmlHttp.send();
+
+        var suggestView3 = new ymaps.SuggestView('stDeparture', {
+            results: 7,
+            provider: {
+                suggest: function (request, options) {
+                    var res = find(Object.keys(flightRouteData['arrayStDeparture']), request),
+                        arrayResult = [],
+                        results = Math.min(options.results, res.length);
+                    for (var i = 0; i < results; i++) {
+                        arrayResult.push({ displayName: res[i], value: res[i] })
+                    }
+                    return ymaps.vow.resolve(arrayResult);
+                }
+            }
         });
-        var suggestView3 = new ymaps.SuggestView('stArrival', {
-            results: 5,
-            provider: getDataProvider(stArrival)
+    });
+}
+function getSuggestionsOfStArrival(stArrival) {
+    ymaps.ready(function init() {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                flightRouteData['arrayStArrival'] = JSON.parse(xmlHttp.responseText)
+            }
+        }
+        xmlHttp.open("GET", apiURL + `/stations?city="${stArrival}"`, true);
+        xmlHttp.send();
+
+        var suggestView4 = new ymaps.SuggestView('stArrival', {
+            results: 7,
+            provider: {
+                suggest: function (request, options) {
+                    var res = find(Object.keys(flightRouteData['arrayStArrival']), request),
+                        arrayResult = [],
+                        results = Math.min(options.results, res.length);
+                    for (var i = 0; i < results; i++) {
+                        arrayResult.push({ displayName: res[i], value: res[i] })
+                    }
+                    return ymaps.vow.resolve(arrayResult);
+                }
+            }
         });
-        var suggestView4 = new ymaps.SuggestView('trainFlight', {
+    });
+}
+function getSuggestionsOfTrainFlight(trainFlightFrom, trainFlightTo) {
+    ymaps.ready(function init() {
+        var arr = [];
+
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                arr = JSON.parse(xmlHttp.responseText)
+                console.log(arr);
+            }
+        }
+        xmlHttp.open("POST", apiURL + `/routes?start_point=${trainFlightFrom}&end_point=${trainFlightTo}`, true);
+        // xmlHttp.open("POST", apiURL + `/routes?start_point=c147&end_point=c213`, true);
+        xmlHttp.send();
+
+        var suggestView5 = new ymaps.SuggestView('trainFlight', {
             results: 5,
-            provider: getDataProvider(trainFlight)
+            provider: {
+                suggest: function (request, options) {
+                    var res = find(Object.values(arr), request),
+                        arrayResult = [],
+                        results = Math.min(options.results, res.length);
+                    for (var i = 0; i < results; i++) {
+                        arrayResult.push({ displayName: res[i], value: res[i] })
+                    }
+                    return ymaps.vow.resolve(arrayResult);
+                }
+            }
         });
     });
 }
